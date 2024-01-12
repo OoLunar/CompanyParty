6fd8b67ae399c15208911be640c27dd646595813
Lunar Starstrum <lunar@forsaken-borders.net>
2024-01-11 23:38:38 -0600

    Hello World

---
+++ CompanyParty/Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace OoLunar.CompanyParty
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public sealed class CompanyPartyPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"{MyPluginInfo.PLUGIN_NAME} has loaded!");
+        }
+    }
+}
